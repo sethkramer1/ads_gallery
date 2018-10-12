d908b5ef161fbfdd21c8c64bc138db9eeb35b5d7
Seth Kramer <sethaepiuva@gmail.com>
2018-10-12 18:14:03 -0400

    added views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,58 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
+
+
+
+
   def index
+    @posts = Post.all.order("created_at DESC")
   end
+
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+
+  def new
+    @post = Post.new
+  end
+
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+
+  def edit
+  end
+
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+
+  end
+
+  def post_params
+    params.require(:post).permit(:company, :description)
+  end
+
+
 end
