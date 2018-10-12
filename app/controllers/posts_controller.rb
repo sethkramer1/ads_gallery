386a792a734c0ead84852d32ab7937d63636eb16
Seth Kramer <sethaepiuva@gmail.com>
2018-10-12 18:31:32 -0400

    created users and set up devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
 
 
@@ -16,12 +17,12 @@ class PostsController < ApplicationController
 
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
     if @post.save
       redirect_to @post
