d0d15b27d24128ad2de57c966a2161dd38b4e598
Seth Kramer <sethaepiuva@gmail.com>
2018-10-13 01:29:07 -0400

    added search, added styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,12 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.order("created_at DESC")
+
+    if params[:search]
+    @posts = Post.search(params[:search]).order("created_at DESC")
+  else
+    @posts = Post.all.order('created_at DESC')
+  end
   end
 
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,13 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image, styles: { medium: "700x500>"}, default_url: "/images/:style/missing.png"
  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+
+
+
+ def self.search(search)
+   where("company LIKE ?", "%#{search}%")
+ end
+
+
 end
